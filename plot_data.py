c518330612883a285269db1773745cad981f8445
Kara Kundert <karakundert@gmail.com>
2013-07-10 13:39:26 -0600

    first draft file to gather stats data from files

---
+++ plot_data.py
@@ -0,0 +1,62 @@
+import sys
+filenames = sys.argv[1:]
+
+import re
+numRegex = r"[+-]?([0-9]+\.)?[0-9]+([eE][+-][0-9]+)?"
+requiredWhiteSpaceRegex = r"[ \t]+"
+optionalWhiteSpaceRegex = r"[ \t]*"
+rmsRegex = r"(?P<rms>%s)" % numRegex
+newline = r"\n"
+headerRegex = (
+        optionalWhiteSpaceRegex
+      + "#"
+      + optionalWhiteSpaceRegex
+      + "Npts"
+      + requiredWhiteSpaceRegex
+      + "Sum"
+      + requiredWhiteSpaceRegex
+      + "Mean"
+      + requiredWhiteSpaceRegex
+      + "Rms"
+      + requiredWhiteSpaceRegex
+      + "Std dev"
+      + requiredWhiteSpaceRegex
+      + "Minimum"
+      + requiredWhiteSpaceRegex
+      + "Maximum"
+      + optionalWhiteSpaceRegex
+)
+regex = (
+        headerRegex
+      + newline
+      + optionalWhiteSpaceRegex
+      + 3 * (numRegex + requiredWhiteSpaceRegex)
+      + rmsRegex
+      + 3 * (requiredWhiteSpaceRegex + numRegex)
+      + optionalWhiteSpaceRegex
+)
+pattern = re.compile(regex, re.IGNORECASE)
+
+rmsValues = []
+rmsValuesNear = []
+rmsValuesOff = []
+for filename in filenames:
+    with open(filename+"/all_stats.txt") as f:
+        contents = f.read()
+        match = pattern.search(contents)
+        assert match
+        rmsValues.append(float(match.group('rms')))
+    with open(filename+"/near_source_stats.txt") as f:
+        contents = f.read()
+        match = pattern.search(contents)
+        assert match
+        rmsValuesNear.append(float(match.group('rms')))
+    with open(filename+"/off_source_stats.txt") as f:
+        contents = f.read()
+        match = pattern.search(contents)
+        assert match
+        rmsValuesOff.append(float(match.group('rms')))
+
+print rmsValues
+print rmsValuesNear
+print rmsValuesOff
