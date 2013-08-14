edac41c51b243baae611adff3eee5bd2f0d003b5
Ken Kundert <thenurd@nurdletech.com>
2013-08-13 17:57:18 -0700

    Initial version of automation.

--- run_test.py
+++ run_test.py
@@ -11,7 +11,37 @@ import matplotlib.pylab as pl
 import random
 import os
 
+class TestSettings:
+    def __init__(self, name, desc, values=[]):
+        self.name = name
+        self.desc = desc
+        self.values = values
+        self.runnum = 0
 
+    def run(self):
+        for val in self.values:
+            print self.desc
+            print "Run number =", self.runnum
+            self.runnum += 1
+            print self.name, "=", val
+            args = {self.name: val}
+            makeMS(runnum="%03d" % self.runnum, **args)
+
+        os.system('rm -rf %s' % self.name)
+        os.system('mkdir %s' % self.name)
+        os.system('mv Data* %s' % self.name)
+
+available_tests = [
+    TestSettings(name='noise', desc='Noise', values=linspace(0.0,0.5,6)),
+    TestSettings(name='supports', desc='Support Beams On/Off', values=[False]),
+]
+
+def run_test(requested_tests=[]):
+    for test in available_tests:
+        if not requested_tests or test.name in requested_tests:
+            test.run()
+
+'''
 def run_test(runnum=0):
 
         for i in linspace(0.0,0.5,6):
@@ -94,3 +124,4 @@ def run_test(runnum=0):
         os.system('rm -rf rot_eccentricity')
         os.system('mkdir rot_eccentricity')
         os.system('mv Data* rot_eccentricity')
+'''
