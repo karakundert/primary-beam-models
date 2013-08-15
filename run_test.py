4ca544f69974553de6c7b179f75011409bf8dbe9
Ken Kundert <thenurd@nurdletech.com>
2013-08-14 17:21:31 -0700

    Added xtransform.

--- run_test.py
+++ run_test.py
@@ -51,12 +51,23 @@ regex = (
 pattern = re.compile(regex, re.IGNORECASE)
 
 class Test:
-    def __init__(self, name, desc, xaxis, settings=[], xlabel=''):
+    def __init__(self, name, desc, xaxis=None, settings=[], xlabel='', xtransform=None):
         self.name = name
         self.desc = desc
         self.settings = settings
         self.xaxis = xaxis
         self.xlabel = xlabel
+        self.xtransform = xtransform
+            # xtransform may be None,
+            #     in which case the value plotted on the x axis is exactly the
+            #     values used in the analysis,
+            # or it may be a number,
+            #     in which case the value used in the plot is the value used
+            #     in the analysis scaled by xtransform,
+            # otherwise xtransform can be a function,
+            #     in which case the value used in the plot is the value returned
+            #     by the function when the value used in the analysis is passed
+            #     in as the only argument.
         self.runnum = 0
 
     def run(self):
@@ -73,6 +84,16 @@ class Test:
 
     def plot(self):
         for level in ['centered', 'half-power,' 'low-power']:
+            if callable(self.transform):
+                xfrm = self.xtransform
+            elif self.transform:
+                xfrm = lambda x: x
+            else:
+                xfrm = lambda x: x*self.transform
+            x_axis = [xfrm(each[self.xaxis]) for each in self.settings]
+            if len(x_axis) < 2:
+                print "plotting of %s skipped" % self.name
+                return
             print "\n###", level.upper()
             filenames = glob('%s/Data*-%s' % (self.name, level))
             rmsValues = []
@@ -100,17 +121,8 @@ class Test:
             print rmsValuesNear
             print rmsValuesOff
 
-            #noise = 50.0
-            #rotate = 45.0
-            #eccentricity = 5.0
-            #phase = 2.0
-
-            #pb_diam = 6.8
-            #phs_off = phase / pb_diam
-
-            x_axis = [each[self.xaxis] for each in self.settings]
             pl.clf()
-            pl.title("%s - %s" % (self.description, level))
+            pl.title("%s - %s" % (self.desc, level))
             p1 = pl.plot(x_axis,rmsValues,'b')
             p2 = pl.plot(x_axis,rmsValuesNear,'g')
             p3 = pl.plot(x_axis,rmsValuesOff,'r')
@@ -120,23 +132,22 @@ class Test:
 
 available_tests = [
     Test(
-        name='noise', desc='Noise', xaxis='noise',
+        name='noise', desc='Noise', xaxis='noise', xtransform=100,
         settings=[{'noise': val} for val in linspace(0.0,0.5,6)],
         xlabel="Percent Increase in Semimajor Axis"),
     Test(
-        name='supports', desc='Support Beams On/Off', xaxis='supports',
-        settings=[{'supports': False}],
-        xlabel="Percent Increase in Semimajor Axis"),
+        name='supports', desc='Support Beams On/Off',
+        settings=[{'supports': False}]),
     Test(
         name='rotate', desc='Rotation Angle', xaxis='rotate',
         settings=[{'rotate': val} for val in linspace(0,45,10)],
         xlabel="Percent Increase in Semimajor Axis"),
     Test(
-        name='eccentricity', desc='Eccentricity', xaxis='ell_u',
+        name='eccentricity', desc='Eccentricity', xaxis='ell_u', xtransform=lambda x: 10*(x-1),
         settings=[{'ell_u': val} for val in linspace(1.0,1.05,6)],
         xlabel="Percent Increase in Semimajor Axis"),
     Test(
-        name='pointing', desc='Pointing Offset', xaxis='offset_u',
+        name='pointing', desc='Pointing Offset', xaxis='offset_u', xtransform=1/6.8,
         settings=[{'offset_u': val} for val in linspace(0,2.0,10)],
         xlabel="Percent Increase in Semimajor Axis"),
     Test(
@@ -146,13 +157,13 @@ available_tests = [
             for offset_u, ell_u in zip(
                     linspace(0,2.0,10), 
                     [1.00, 1.01, 1.02, 1.03, 1.04, 1.05])],
-        xlabel="Percent Increase in Semimajor Axis"),
+        xlabel="Percent Increase in Semimajor Axis")
 ]
 
-def run_test(requested_tests=[], run=True, plot=True):
+def run_test(tests=[], run=True, plot=True):
     assert run or plot
     for test in available_tests:
-        if not requested_tests or test.name in requested_tests:
+        if not tests or test.name in tests:
             if run:
                 test.run()
             if plot:
