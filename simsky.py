aece0cf79e3a5d2493a73f0883d4c66f89205780
Ken Kundert <thenurd@nurdletech.com>
2013-12-13 11:59:41 -0600

    Added comments.

--- simsky.py
+++ simsky.py
@@ -1,7 +1,10 @@
-###
-### Simulate Sky and Data...
-###
+### Simulate Sky and Data ###
+#
+# Kara Kundert (karakundert@gmail.com)
+# Dec 2013
 
+
+# Imports {{{1
 from numpy import *
 from scipy import *
 from scipy import fftpack
@@ -14,20 +17,24 @@ import matplotlib.pylab as pl
 import random
 import time
 
+
+# Globals {{{1
 sourcedir = '/lustre/kkundert/Code/'
 
+
+# Includes {{{1
 execfile(sourcedir+'mksrc.py')
 execfile(sourcedir+'parang.py')
 execfile(sourcedir+'writeAps.py')
 execfile(sourcedir+'makeBeams.py')
 
-###############################################
 
+# makeMS() {{{1
 def makeMS(runnum=0, makeBeams = True,
            noise=0.0, supports=True,
            ell_u = 1.0, ell_v = 1.0,
            pointing = False, theta = 0.0):
-
+    """Run simulation."""
 
   for i in xrange(1):
       dirname = "Data"+str(runnum)
@@ -82,7 +89,7 @@ def makeMS(runnum=0, makeBeams = True,
           os.system('cp -r '+msname+' ap.ms')
           writeApertures(msname='ap.ms',imname=imname)
           print "apertures written"
-          
+
           Nxy_list = []
           for i in xrange(num_ant):
               print "aper" + str(i)
@@ -165,27 +172,41 @@ def makeMS(runnum=0, makeBeams = True,
 
   ##return [stats0,stats1,stats2]
 
-###############################################
 
+# makeBeamDifference() {{{1
 def makeBeamDifference(pb1="primary-beam-model",pb2="primary-beam-perturbed"):
+    """Create file that contains difference between two beam models.
 
+    Arguments:
+    pb1='primary-beam-model'
+        File name that contains reference beam.
+    pb2='primary-beam-perturbed'
+        File name that contains beam to subtract from the reference beam.
+
+    Results:
+    A file named 'pbdiff' is created that contains the difference.
+    """
+
+    # Read reference beam file
     ia.open(pb1)
     pix1 = ia.getchunk()
     ia.close()
 
+    # Read perturbed beam file
     ia.open(pb2)
     pix2 = ia.getchunk()
     ia.close()
 
+    # Create empty image file named 'pbdiff'
     os.system('cp -r '+pb1+' pbdiff')
 
+    # Write the difference into 'pbdiff'
     ia.open("pbdiff")
     ia.putchunk(pix1-pix2)
     ia.close()
 
-###############################################
 
-
+# makeMSFrame() {{{1
 def makeMSFrame(dirname,msname,ra0,dec0,nchan):
   msstokes='RR LL';
   feedtype='perfect R L';
@@ -241,8 +262,8 @@ def makeMSFrame(dirname,msname,ra0,dec0,nchan):
 
   return d
 
-###############################################
 
+# makeResidualImage() {{{1
 def makeResidualImage(msname='',resname='',imsize=256,cellsize='8.0arcsec',
                       ra0='', dec0='', nchan=1, reffreq='1.5GHz'):
   ## Make model image
@@ -256,8 +277,8 @@ def makeResidualImage(msname='',resname='',imsize=256,cellsize='8.0arcsec',
     im.makeimage(type="residual",image=resname);
     im.done();
 
-###############################################
 
+# makeImage() {{{1
 def makeImage(msname='',imname='',clname='mysource.cl',
               imsize=256,cellsize='8.0arcsec',ra0='',dec0='',
               nchan=1,reffreq='1.5GHz'):
@@ -279,8 +300,8 @@ def makeImage(msname='',imname='',clname='mysource.cl',
   ia.modify(model=cl.torecord(),subtract=False);
   cl.close();
 
-###############################################
 
+# makeTrueImage() {{{1
 def makeTrueImage(stokesvals=[1.0,0.0,0.0,1.0],imname='',newimname='',
                    pb_real_file = "pb00&&01real",
                    pb_imag_file = "pb00&&01imag",
@@ -306,7 +327,7 @@ def makeTrueImage(stokesvals=[1.0,0.0,0.0,1.0],imname='',newimname='',
 
       time2 = time.time()
       print "open files: " + str(time2-time1)
-        
+
       tb.open(msname)
       tb1 = tb.query('SCAN_NUMBER=='+str(scanid+1))
       timelist = tb1.getcol('TIME')
@@ -364,10 +385,10 @@ def makeTrueImage(stokesvals=[1.0,0.0,0.0,1.0],imname='',newimname='',
       time6 = time.time()
       print "prediction: " + str(time6-time5)
 
-#####################################
 
+# makeDataTable() {{{1
 def makeDataTable(msname=''):
-  
+
   ### Copy to the data and corrected-data columns
   tb.open(msname,nomodify=False);
   moddata = tb.getcol(columnname='MODEL_DATA');
@@ -378,61 +399,70 @@ def makeDataTable(msname=''):
   tb.putcol(columnname='MODEL_DATA',value=moddata);
   tb.close();
 
-#####################################
 
+# addNoise() {{{1
 def addNoise(msname=''):
   sm.openfromms(msname);
   sm.setnoise(mode='simplenoise',simplenoise='0.3Jy');
   sm.corrupt();
   sm.close();
 
-###############################################
 
+# getAntLocations() {{{1
 def getAntLocations(configuration='D'):
-   vx = [41.1100006,  134.110001,   268.309998,  439.410004,  644.210022,
-         880.309998,  1147.10999,  1442.41003,  1765.41003,  -36.7900009,
-         -121.690002,  -244.789993, -401.190002, -588.48999,  -804.690002,
-         -1048.48999, -1318.48999, -1613.98999,  -4.38999987,-11.29,  
-         -22.7900009, -37.6899986, -55.3899994, -75.8899994, -99.0899963, 
-         -124.690002, -152.690002];
-   vy = [3.51999998, -39.8300018,  -102.480003, -182.149994, -277.589996,
-        -387.839996, -512.119995, -649.76001,  -800.450012, -2.58999991,
-        -59.9099998,  -142.889999, -248.410004, -374.690002, -520.599976,
-        -685,        -867.099976, -1066.42004,   77.1500015,  156.910004, 
-        287.980011,  457.429993,  660.409973,  894.700012,  1158.82996, 
-        1451.43005,  1771.48999];
-   vz = [0.25,       -0.439999998, -1.46000004, -3.77999997, -5.9000001,
-        -7.28999996, -8.48999977, -10.5,       -9.56000042,      0.25, 
-        -0.699999988, -1.79999995, -3.28999996, -4.78999996, -6.48999977,
-        -9.17000008, -12.5299997, -15.3699999,  1.25999999,   2.42000008, 
-        4.23000002,  6.65999985,  9.5,         12.7700005,  16.6800003, 
-        21.2299995,  26.3299999];
-   d = [25.0,       25.0,         25.0,         25.0,       25.0,
-        25.0,       25.0,         25.0,         25.0,       25.0,
-        25.0,       25.0,         25.0,         25.0,       25.0,
-        25.0,       25.0,         25.0,         25.0,       25.0,
-        25.0,       25.0,         25.0,         25.0,       25.0,
-        25.0,       25.0];
-   if(configuration=='D'):
-      scale = 3.0;
-   else:
-     if(configuration=='A'):
-       scale = 1.0/9.0;
-     else:
-       scale = 1.0;
-       print 'Using VLA C-array coords';
-   nn = len(vx);
-   x = (vx - (sum(pl.array(vx))/(nn)))/scale;
-   y = (vy - (sum(pl.array(vy))/(nn)))/scale;
-   z = (vz - (sum(pl.array(vz))/(nn)))/scale;
-   an=[];
-   for i in range(0,nn):
-           an.append("VLA"+str(i));
-   return d,an,x,y,z
+    """Return location information for each antenna in array.
 
+    Arguments:
+        Takes configuration as argument (choose from 'A', 'C', 'D').
+    Returns:
+        Tuple of arrays containing antenna diameters (m), names,
+        and x, y, and z locations (m).
+    """
 
+    # Raw antenna locations in C configuration
+    vx = [41.1100006,  134.110001,   268.309998,  439.410004,  644.210022,
+          880.309998,  1147.10999,  1442.41003,  1765.41003,  -36.7900009,
+          -121.690002,  -244.789993, -401.190002, -588.48999,  -804.690002,
+          -1048.48999, -1318.48999, -1613.98999,  -4.38999987,-11.29,  
+          -22.7900009, -37.6899986, -55.3899994, -75.8899994, -99.0899963, 
+          -124.690002, -152.690002]
+    vy = [3.51999998, -39.8300018,  -102.480003, -182.149994, -277.589996,
+         -387.839996, -512.119995, -649.76001,  -800.450012, -2.58999991,
+         -59.9099998,  -142.889999, -248.410004, -374.690002, -520.599976,
+         -685,        -867.099976, -1066.42004,   77.1500015,  156.910004, 
+         287.980011,  457.429993,  660.409973,  894.700012,  1158.82996, 
+         1451.43005,  1771.48999]
+    vz = [0.25,       -0.439999998, -1.46000004, -3.77999997, -5.9000001,
+         -7.28999996, -8.48999977, -10.5,       -9.56000042,      0.25, 
+         -0.699999988, -1.79999995, -3.28999996, -4.78999996, -6.48999977,
+         -9.17000008, -12.5299997, -15.3699999,  1.25999999,   2.42000008, 
+         4.23000002,  6.65999985,  9.5,         12.7700005,  16.6800003, 
+         21.2299995,  26.3299999]
+    d = [25.0,       25.0,         25.0,         25.0,       25.0,
+         25.0,       25.0,         25.0,         25.0,       25.0,
+         25.0,       25.0,         25.0,         25.0,       25.0,
+         25.0,       25.0,         25.0,         25.0,       25.0,
+         25.0,       25.0,         25.0,         25.0,       25.0,
+         25.0,       25.0]
 
+    # Get scaling factor for antenna locations based on configuration
+    if(configuration=='D'):
+        scale = 3.0
+    else:
+        if(configuration=='A'):
+            scale = 1.0/9.0
+        else:
+            scale = 1.0
+        print 'Using VLA C-array coords'
 
-###############
-####  Imaging
-###############
+    # Move antennas into desired configuration
+    nn = len(vx)
+    x = (vx - (sum(pl.array(vx))/(nn)))/scale
+    y = (vy - (sum(pl.array(vy))/(nn)))/scale
+    z = (vz - (sum(pl.array(vz))/(nn)))/scale
+
+    # Label the antenna
+    an=[]
+    for i in range(0,nn):
+        an.append("VLA"+str(i))
+    return d,an,x,y,z
